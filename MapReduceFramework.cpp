75548195fdacb1afe4faf3bc7fe9d9673ef27cf8
rawaa <rawaa.aburaya@mail.huji.ac.il>
2024-07-02 15:58:09 +0300

    ex3

--- MapReduceFramework.cpp
+++ MapReduceFramework.cpp
@@ -34,7 +34,9 @@ struct Job_context{
     Barrier *barrier;
     Thread_context thread_context;
     const MapReduceClient *client;
-    std::vector<IntermediateVec> vec_of_vecs;
+    std::vector<IntermediateVec> vec_of_inter_vecs;
+    std::vector<IntermediateVec> shuffled_vecs;
+
     std::atomic<int>mapcompleted;
     std::atomic<int>reducecompleted;
     std::atomic<int>total_pairs;
@@ -44,18 +46,18 @@ struct Job_context{
 };
 //TODO checkthis function
 void* mapPhase(void* arg) {
-    Job_context* jobContext = static_cast<Job_context*>(arg);
+    Thread_context* thread_context = static_cast<Thread_context*>(arg);
     //auto *jobContext = (Job_context*)threadContext->;
-    for (const auto& inputPair : jobContext->input_vec) {
-        if(pthread_mutex_lock(&jobContext->map_mutex)!=0){
+    for (const auto& inputPair : thread_context->job_context->input_vec) {
+        if(pthread_mutex_lock(&thread_context->job_context->map_mutex)!=0){
             printf("ERROR");//TODO
             printf("\n");
             exit(EXIT_FAILURE);
         }
         // momken bdl jobcontext tkon intermediate vec
-        jobContext->client->map(inputPair.first, inputPair.second, jobContext);
-        jobContext->mapcompleted++;
-        if(pthread_mutex_unlock(&jobContext->map_mutex)!=0){
+        thread_context->job_context->client->map(inputPair.first, inputPair.second, thread_context->job_context);
+        thread_context->job_context->mapcompleted++;
+        if(pthread_mutex_unlock(&thread_context->job_context->map_mutex)!=0){
             printf("ERROR");//TODO
             printf("\n");
             exit(EXIT_FAILURE);
@@ -72,12 +74,39 @@ bool sort_helper(IntermediatePair &first, IntermediatePair &sec){
 
 void* sort_vec(Thread_context* threadContext){
     auto *jobContext = (Job_context*)threadContext->job_context;
-    IntermediateVec &curr = jobContext->vec_of_vecs.at(threadContext->tid);
+    IntermediateVec &curr = jobContext->vec_of_inter_vecs.at(threadContext->tid);
     std::sort(curr.begin(),curr.end(),sort_helper);
 }
 
 
-void suffle_vectors(void* arg){
+//void suffle_vectors(void* arg){
+//
+//    while (!allIntermediateVectorsEmpty()) {
+//            std::vector<std::pair<K2, V2>> newVector;
+//            for (auto &intermediateVector : intermediateVectors) {
+//                if (!intermediateVector.empty()) {
+//                    newVector.push_back(intermediateVector.back());  // Access the last element
+//                    intermediateVector.pop_back();  // Remove the last element
+//                }
+//            }
+//            queueOfVectors.push_back(newVector);
+//            vectorCounter++;
+//        }
+//    }
+//
+//}
+void shuffle_phase(void* arg) {
+    Thread_context* thread_context = (Thread_context*) arg;
+    std::vector<std::pair<K2, V2>> new_vec;
+    auto curr = thread_context->job_context->intermediate_vec.back();
+
+    while (!thread_context->job_context->vec_of_inter_vecs.empty()) {
+
+       // if(!(*(curr.first)<*()))
+
+
+    }
+
 
 }
 
@@ -100,4 +129,12 @@ JobHandle startMapReduceJob(const MapReduceClient& client,
     }
     return job;
 }
+
+
+
+void* map_reduce_job(void* arg){
+    Job_context* jobContext = static_cast<Job_context*>(arg);
+
+}
+
 void waitForJob(JobHandle job){}
